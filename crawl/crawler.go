09147d91cf9ed1cc54510f34fcbd0a21ea854356
EvilScott <reis.robert.s@gmail.com>
2017-08-17 17:45:31 -0700

    some refactoring to get routines to work

--- crawl/crawler.go
+++ crawl/crawler.go
@@ -2,21 +2,21 @@ package crawl
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
-	"sync"
 
 	"golang.org/x/net/html"
 
 	"github.com/evilscott/gocrawler/types"
 )
 
-func grabLinks(baseURL *url.URL, resp *http.Response) []string {
+func grabLinks(baseURL *url.URL, body io.ReadCloser) []string {
 	// keep track of urls on page
 	linkMap := types.NewURLSet()
 
 	// walk elements
-	z := html.NewTokenizer(resp.Body)
+	z := html.NewTokenizer(body)
 	for tt := z.Next(); tt != html.ErrorToken; tt = z.Next() {
 		// skip anything that isn't a starting tag
 		if tt != html.StartTagToken {
@@ -46,34 +46,35 @@ func grabLinks(baseURL *url.URL, resp *http.Response) []string {
 	return linkMap.Slice()
 }
 
-func Crawler(id int, scheme, domain string, todoURLs <-chan string, foundURLs chan<- string, wg sync.WaitGroup) {
-	for path := range todoURLs {
-		// signal that work is happening
-		wg.Add(1)
+func Crawler(id int, scheme, domain string, todos <-chan string, found chan<- string) {
+	for {
+		select {
+		case path := <- todos:
+			// create url from domain and path
+			target := fmt.Sprintf("%s://%s%s", scheme, domain, path)
+			base, err := url.Parse(target)
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
 
-		// create url from domain and path
-		targetURL := fmt.Sprintf("%s://%s%s", scheme, domain, path)
-		baseURL, err := url.Parse(targetURL)
-		if err != nil {
-			fmt.Println(err.Error())
-			continue
+			// hit URL
+			fmt.Printf("Crawler #%d crawling %s ...\n", id, target)
+			resp, err := http.Get(target)
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+
+			// grab links and send them to channel
+			for _, link := range grabLinks(base, resp.Body) {
+				found <- link
+			}
+
+			// close response body and signal work is done
+			resp.Body.Close()
+
+		default: // noop
 		}
-
-		// hit URL
-		fmt.Printf("Crawler #%d crawling %s ...\n", id, targetURL)
-		resp, err := http.Get(targetURL)
-		if err != nil {
-			fmt.Println(err.Error())
-			continue
-		}
-
-		// grab links and send them to channel
-		for _, found := range grabLinks(baseURL, resp) {
-			foundURLs <- found
-		}
-
-		// close response body and signal work is done
-		resp.Body.Close()
-		wg.Done()
 	}
 }
--- main/main.go
+++ main/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/url"
 	"os"
-	"sync"
 
 	"github.com/evilscott/gocrawler/crawl"
 	"github.com/evilscott/gocrawler/types"
@@ -12,40 +11,42 @@ import (
 
 func main() {
 	// parse initial url
-	startURL, err := url.Parse(os.Args[1])
+	start, err := url.Parse(os.Args[1])
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
 
 	// url set
-	foundLinks := types.NewURLSet()
+	results := types.NewURLSet()
 
 	// channels
-	todoURLs := make(chan string)
-	foundURLs := make(chan string)
-
-	// wait group
-	var wg sync.WaitGroup
+	todos := make(chan string, 100)
+	found := make(chan string, 100)
 
 	// crawler workers
 	// TODO configurable number of workers via args
 	for i := 1; i <= 3; i++ {
-		go crawl.Crawler(i, startURL.Scheme, startURL.Hostname(), todoURLs, foundURLs, wg)
+		go crawl.Crawler(i, start.Scheme, start.Hostname(), todos, found)
 	}
 
 	// listening for crawler results
 	fmt.Println("Starting crawl ...")
-	todoURLs <- startURL.Path
-	for found := range foundURLs {
-		wg.Add(1)
-		if !foundLinks.AddURL(found) {
-			todoURLs <- found
+	todos <- start.Path
+	results.AddURL(start.Path)
+
+	// main loop
+	for {
+		select {
+		case link := <-found:
+			if !results.AddURL(link) {
+				todos <- link
+			}
+		default:
+			// TODO check for end condition
 		}
-		wg.Done()
 	}
 
 	// wait for all workers to finish
-	wg.Wait()
-	fmt.Printf("Finished! Links found:%s\n", foundLinks.String())
+	fmt.Printf("Finished! Links found:%s\n", results.String())
 }
